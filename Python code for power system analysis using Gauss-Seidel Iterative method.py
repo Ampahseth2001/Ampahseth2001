56c2c55e31759b17a7bc7f6537eae205eca106bb
Ampahseth2001 <134621306+Ampahseth2001@users.noreply.github.com>
2023-05-31 10:03:13 +0100

    Update Python code for power system analysis using Gauss-Seidel Iterative method.py

--- Python code for power system analysis using Gauss-Seidel Iterative method.py
+++ Python code for power system analysis using Gauss-Seidel Iterative method.py
@@ -2,11 +2,11 @@
 
 import numpy as np
 
-n= int(input('The number of buses?: '))
+n= int(input('Enter number of buses?: ')) #takes number of busbars as input from the user
 rows,columns= n,n
 
 big=[]
-for i in range(1, rows+1):
+for i in range(1, rows+1): #Allows the user to enter admittances as matrix input
     small=[]
     for k in range(1, columns+1):
         Y= complex(input(f'Y{i}{k} = '))
@@ -15,20 +15,20 @@ for i in range(1, rows+1):
     big.append(small)
 
 
-Ybus= np.array(big)
+Ybus= np.array(big) #Convert the list "big" into a matrix
 print(Ybus)
 
 print('\n\t\t\tIntital Voltage, Supply and Demand Values')
 Voltages, Supply, Demand= [],[],[]
-for i in range(1,1+n):
+for i in range(1,1+n): #Allows the user to input the given Slack bus voltage(V1) and assume initial values for V2,V3...Vn 
     V= complex(input(f'Voltage{i}= '))
     Voltages.append(V)
 
-for i in range(1,1+n):
+for i in range(1,1+n): #Allows the user to input the given values for Supply Apparent Power as Si= Pi+jQi
     S= complex(input(f'Supply{i}= '))
     Supply.append(S)
 
-for i in range(1,1+n):
+for i in range(1,1+n): #Allows the user to input the given values for Demand Apparent Power as Si= Pi+jQi
     D= complex(input(f'Demand{i}= '))
     Demand.append(D)
 
@@ -37,6 +37,7 @@ print(Supply)
 print(Demand)
 
 def summation(V,Y,n,k):
+    """ A function to calculate the summation part of the formula """
     out=0
     for a in range(n):
         if a!=k:
@@ -44,7 +45,7 @@ def summation(V,Y,n,k):
 
     return out
 
-Iter= int(input('\nNumber of iterations:'))
+Iter= int(input('\nNumber of iterations:')) # Allows you to enter the number of iterations
 
 final_answer={}
 for i in range(Iter):
@@ -52,12 +53,12 @@ for i in range(Iter):
     for k in range(n):
         zigma= summation(Voltages,big,n,k)
         if k==0:
-            Voltages[k]=Voltages[0]
+            Voltages[k]=Voltages[0] # since Slack bus voltage remains constant for every iteration
         else:
-            Voltages[k]= (((Supply[k]-Demand[k]).conjugate())/(Voltages[k].conjugate())-(zigma))/big[k][k]
+            Voltages[k]= (((Supply[k]-Demand[k]).conjugate())/(Voltages[k].conjugate())-(zigma))/big[k][k] #Formula
         values.append(Voltages[k])
 
     final_answer[f'{i+1}']=values
 
-for key,value in final_answer.items():
+for key,value in final_answer.items(): # Dictionary allows the user to display the values after each iteration
     print(f'Iteration{key}: {value}')
